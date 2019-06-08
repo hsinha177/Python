b8bf12ba79bd9fe9047ee948a2c594aa734b0c6e
hsinha177 <51314890+hsinha177@users.noreply.github.com>
2019-06-08 14:46:35 +0530

    to find the size of datatype

---
+++ size_datatyp.py
@@ -0,0 +1,21 @@
+#! /usr/bin/python3
+
+import sys
+
+x=10
+y='Harsh'
+z=242.46
+
+print(type(x))
+print(" X of size : ")
+print(sys.getsizeof(x))
+print("\n")
+print(type(y))
+print(" Y of size : ")
+print(sys.getsizeof(y))
+print("\n")
+print(type(z))
+print(" Z of size : ")
+print(sys.getsizeof(z))
+
+
