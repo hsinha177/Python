ef3612383acd66051a8bac61c3aaffa2d1d562c4
root <root@ip-172-31-15-127.ap-south-1.compute.internal>
2019-06-13 07:15:10 +0000

    Pgm to perform cat operation

---
+++ cat.py
@@ -0,0 +1,26 @@
+#Python Program which performs similar operation like "CAT" shell command
+c=int(input("Enter the command : \n 1. Cat \t2.Cat -b\t3.Cat -s\t4.Cat -E\n:")) 
+
+#OPEN the FILE 
+file=open("demofile.txt","r") 
+
+#Normal Reading like cat function 
+if choice==1: 
+	print(file.read()) 
+elif choice==2: 
+	i=1 
+	for lines in file:
+		if lines.strip() !="": 
+			print(str(i)+". " +lines.strip()) 
+			i+=1 
+elif choice==3: 
+	for lines in file: 
+		if lines.strip() !="": 
+			print(lines.strip()) 
+elif choice==4: 
+	for lines in file: 
+		print(lines.strip() +"$") 
+else: 
+	print("Enter choice from given inputs") 
+
+file.close()
