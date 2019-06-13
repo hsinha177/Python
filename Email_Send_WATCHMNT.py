82e3c3512ed50b8da950cb123a08d1d00b528261
Cloud User <ec2-user@ip-172-31-15-127.ap-south-1.compute.internal>
2019-06-13 14:24:57 +0000

    Send email without attachment

---
+++ Email_Send_WATCHMNT.py
@@ -0,0 +1,32 @@
+import smtplib 
+from email.mime.text import MIMEText 
+from email.mime.multipart import MIMEMultipart 
+from email.mime.base import MIMEBase 
+from email import encoders 
+
+#user info sender to receiver
+email_user = 'kgfgoel@gmail.com' 
+email_password = '0919rebel' 
+email_send = 'hsinha177@gmail.com' 
+subject = 'Email test' 
+
+#message format
+msg = MIMEMultipart() 
+msg['From'] = email_user 
+msg['To'] = email_send 
+msg['Subject'] = subject 
+
+#email body content
+body = 'Hello ! this message is sent from sid to shubh via python code' 
+
+msg.attach(MIMEText(body,'plain')) 
+
+#converting msg as string type
+text=msg.as_string()
+
+#protocols to send over smtp 
+server = smtplib.SMTP('smtp.gmail.com',587) 
+server.starttls() 
+server.login(email_user,email_password) 
+server.sendmail(email_user,email_send,text) 
+server.quit()
